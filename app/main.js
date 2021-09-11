42be40df6f9e1a9ceeb95ea7342c70b122306192
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2021-09-11 16:03:38 +0530

    upgrade to https server

--- app/main.js
+++ app/main.js
@@ -1,15 +1,22 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
 import { requestValidator } from '../validators/requestValidator.js';
 import { getResponse } from '../controller/itemController.js';
 
 const app = express();
-const port = 3000;
+const port = 3443;
 
 app.get('/ledger/', requestValidator(), getResponse());
 
-const server = app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+var httpsServer = https.createServer(options, app);
+const server = httpsServer.listen(port, () => {
+    console.log(`Example app listening at https://localhost:${port}`)
 });
 
 export { server };
-
--- test/test.js
+++ test/test.js
@@ -9,18 +9,21 @@ describe('loading express', function () {
   it('Test successful API call', function testLedgerEP(done) {
     supertest.agent(server)
       .get('/ledger?start_date=2020-01-31&end_date=2021-03-31&frequency=MONTHLY&weekly_rent=555')
+      .trustLocalhost()
       .expect(200, done);
   });
 
   it('Test Missing Query Params', function testLedgerEP(done) {
     supertest.agent(server)
       .get('/ledger?start_date=2020-01-31&frequency=MONTHLY&weekly_rent=555')
+      .trustLocalhost()
       .expect(400, done);
   });
 
   it('404 everything else', function testInvalidPath(done) {
     supertest.agent(server)
       .get('/foo/bar')
+      .trustLocalhost()
       .expect(404, done);
   });
 
